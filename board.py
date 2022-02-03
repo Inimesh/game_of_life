7b84c9ad9d04a5c2ad7d0e3e93cb9c71b9b082e6
Inimesh <ijmeshoulam@gmail.com>
2022-02-03 18:09:15 +0000

    Feat: Board.populate method populates self.grid with ones at specified indices

--- board.py
+++ board.py
@@ -4,6 +4,15 @@ class Board:
     self.height = height
     self.grid = [[0 for j in range(self.width)] for i in range(self.height)]
 
-  def pretty_grid(self) -> str:
+  def pretty_grid(self):
+    """A method that returns a formatted string of self.grid"""
     formatted_grid = ('\n'.join([' '.join([str(cell) for cell in row]) for row in self.grid]))
     return formatted_grid
+
+  def populate(self, positions):
+    """A setter method that populates self.grid with ones at specified 2D indices"""
+    for index_set in positions:
+      i, j = index_set
+      self.grid[i][j] = 1
+  
+  
--- board_test.py
+++ board_test.py
@@ -1,8 +1,21 @@
 from board import Board
 
 def test_pretty_grid():
+  """pretty_grid() should return a formatted string of self.grid"""
   board = Board(5, 5)
   assert board.pretty_grid() == "0 0 0 0 0\n0 0 0 0 0\n0 0 0 0 0\n0 0 0 0 0\n0 0 0 0 0"
+
+def test_populate():
+  """populate() should populate self.grid with ones as the specified 2D indices [[i,j], [i,j]...]"""
+  board = Board(5, 5)
+
+  positions = [ # Glider config
+    [1,2],
+    [2,0],[2,2],
+    [3,1],[3,2]
+  ]
+  board.populate(positions)
+  assert board.pretty_grid() == "0 0 0 0 0\n0 0 1 0 0\n1 0 1 0 0\n0 1 1 0 0\n0 0 0 0 0"
   
 
 
