bd557db6bc30ab1d74fa25b2a58b59f9dd9e050a
Inimesh <ijmeshoulam@gmail.com>
2022-02-04 10:40:17 +0000

    Testing: rewtriting tests in board_test.py to use unittest module instead of pytest

--- board_test.py
+++ board_test.py
@@ -1,23 +1,30 @@
+import unittest
 from board import Board
 
-def test_pretty_grid():
-  """pretty_grid() should return a formatted string of self.grid"""
-  board = Board(5, 5)
-  assert board.pretty_grid() == "0 0 0 0 0\n0 0 0 0 0\n0 0 0 0 0\n0 0 0 0 0\n0 0 0 0 0"
+class TestBoard(unittest.TestCase):
 
-def test_populate():
-  """populate() should populate self.grid with ones as the specified 2D indices [[i,j], [i,j]...]"""
-  board = Board(5, 5)
+  def setUp(self):
+    self.board = Board(5, 5)
 
-  positions = [ # Glider config
-    [1,2],
-    [2,0],[2,2],
-    [3,1],[3,2]
-  ]
-  board.populate(positions)
-  assert board.pretty_grid() == "0 0 0 0 0\n0 0 1 0 0\n1 0 1 0 0\n0 1 1 0 0\n0 0 0 0 0"
-  
+  def tearDown(self):
+    pass
 
+  def test_pretty_grid(self):
+    """pretty_grid() should return a formatted string of self.grid"""
+    self.assertEqual(self.board.pretty_grid(), "0 0 0 0 0\n0 0 0 0 0\n0 0 0 0 0\n0 0 0 0 0\n0 0 0 0 0")
+
+  def test_populate(self):
+    """populate() should populate self.grid with ones as the specified 2D indices [[i,j], [i,j]...]"""
+    positions = [ # Glider config
+      [1,2],
+      [2,0],[2,2],
+      [3,1],[3,2]
+    ]
+    self.board.populate(positions)
+    self.assertEqual(self.board.pretty_grid(), "0 0 0 0 0\n0 0 1 0 0\n1 0 1 0 0\n0 1 1 0 0\n0 0 0 0 0")
+    
+if __name__ == '__main__':
+  unittest.main()
 
 
 
